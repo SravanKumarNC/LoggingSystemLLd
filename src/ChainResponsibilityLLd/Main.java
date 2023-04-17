e8848539b81d95498d989e4f8a6a14e9bcf1d07b
sravanKumar <sravankumarnc@gmail.com>
2023-04-17 11:13:25 +0530

    logger

---
+++ src/ChainResponsibilityLLd/Main.java
@@ -0,0 +1,11 @@
+package ChainResponsibilityLLd;
+
+public class Main {
+    public static void main(String[] args) {
+        LogProcessor logObject = new InfoLogProcessor(new DebugLogProcessor(new ErrorLogProcessor(null)));
+
+        logObject.log(LogProcessor.ERROR, "exception happened");
+        logObject.log(LogProcessor.DEBUG, "need to debug");
+        logObject.log(LogProcessor.INFO, "just for info");
+    }
+}
